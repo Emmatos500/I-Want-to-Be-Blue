7ac79858bc61af4dac4455471741ade0d2b05af2
Emmett <ekennedy@cps.edu>
2021-07-19 11:12:54 -0500

    Initial Commit

---
+++ I Want to Be Blue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Want to Be Blue
+//
+//  Created by Emmett Kennedy on 7/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
